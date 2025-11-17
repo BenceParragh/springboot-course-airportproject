2100d485002f5d7e30e698ff646b79b8a4ccdd41
DESKTOP-SMP6UEN\parra <parragh4@gmail.com>
2025-11-17 10:23:42 +0100

    deleted files

--- airport/src/main/java/hu/cubix/airport/service/DiscountService.java
+++
@@ -1,6 +0,0 @@
-package hu.cubix.airport.service;
-
-public interface DiscountService {
-
-	public int getDiscountPercent(int totalPrice);
-}
--- hr/src/main/java/hu/cubix/hr/bencepar/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package hu.cubix.hr.bencepar.service;
-
-import hu.cubix.hr.bencepar.model.Employee;
-
-public interface EmployeeService {
-
-	public int getPayRaisePercent(Employee employee);
-
-}
